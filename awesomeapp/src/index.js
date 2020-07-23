b48a22b848188d5aac74c5e8069917966362df2d
karun <karunahuja1998@gmail.com>
2020-07-23 08:25:12 +0530

    JSX in React JS

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -5,4 +5,13 @@ import ReactDOM from 'react-dom';
 
 
 // ReactDOM.render('kya dikhana hai','kahan dikhana hai','callback func');
-ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+ReactDOM.render(<h1>Hello World!!</h1>,
+  document.getElementById('root'));
+// jsx:-javascript exts or javascript xml
+
+
+// Below is another method to render HTML with javascript
+
+// var h1=document.createElement("h1");
+// h1.innerHTML="karun Ahuja";
+// document.getElementById("root").appendChild(h1)
