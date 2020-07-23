16a9722042517c57fc8e9241eb9020548a9a6b36
karun <karunahuja1998@gmail.com>
2020-07-23 22:33:27 +0530

    ES6 Modules Import Export

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -3,13 +3,7 @@ import React from 'react'; //writing in es6 as babel will convert it to browser 
 // var ReactDOM = require('react-dom');
 import ReactDOM from 'react-dom';
 import './index.css'
-import App from './App.jsx';
-
-
-
-
-
-
+import * as ques from './App.jsx';
 
 // ReactDOM.render('kya dikhana hai','kahan dikhana hai','callback func');
 ReactDOM.render(
@@ -18,9 +12,19 @@ ReactDOM.render(
   //We should use React fragment instead of div if version is before 16 as div will create extra div
 
 
+<React.Fragment>
 
-  <App></App>
+  <ol>
+    <li>Karun</li>
+    <li>{ques.default}</li>
+    <li>{ques.favprog}</li>
+    <li>{ques.myName()}</li>
+    <li>{ques.myNames()}</li>
 
+  </ol>
+
+
+</React.Fragment>
 
 
   ,
