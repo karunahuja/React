83e57ab076a20129893a67f7d8c73c251bedf459
karun <karunahuja1998@gmail.com>
2020-07-23 08:49:42 +0530

    Using variable of javascript in jsx of render

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -3,6 +3,7 @@ import React from 'react'; //writing in es6 as babel will convert it to browser 
 // var ReactDOM = require('react-dom');
 import ReactDOM from 'react-dom';
 
+const name="Karun Ahuja";
 
 // ReactDOM.render('kya dikhana hai','kahan dikhana hai','callback func');
 ReactDOM.render(
@@ -11,8 +12,14 @@ ReactDOM.render(
   //We should use React fragment instead of div if version is before 16 as div will create extra div
 
   <React.Fragment>
-    <h1>Hello World!!</h1>
-    <p>The world is here</p>
+    <h1>Hello {name}</h1>
+    <p>List of 5 best series</p>
+    <ol>
+      <li>Dark {2+3}</li>
+      <li>Extra circular</li>
+      <li>Holo</li>
+      <li>Love</li>
+    </ol>
   </React.Fragment>
 
   ,
