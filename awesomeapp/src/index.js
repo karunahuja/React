b307d7053a13a24d9e9ccb1d4b4924945444c963
karun <karunahuja1998@gmail.com>
2020-07-23 08:34:28 +0530

    Render Multiple Elements inside ReactDOM.render()

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -5,13 +5,16 @@ import ReactDOM from 'react-dom';
 
 
 // ReactDOM.render('kya dikhana hai','kahan dikhana hai','callback func');
-ReactDOM.render(<h1>Hello World!!</h1>,
+ReactDOM.render(
+  //div or [ , ] is compulsary for multiple elements
+  [
+  <h1>Hello World!!</h1>,
+  <p>The world is here</p>
+  
+  ]
+
+  ,
+
+
   document.getElementById('root'));
-// jsx:-javascript exts or javascript xml
 
-
-// Below is another method to render HTML with javascript
-
-// var h1=document.createElement("h1");
-// h1.innerHTML="karun Ahuja";
-// document.getElementById("root").appendChild(h1)
