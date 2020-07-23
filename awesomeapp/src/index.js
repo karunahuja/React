e8479c7538f819a9912a6a459efc58870aa3af68
karun <karunahuja1998@gmail.com>
2020-07-23 08:40:08 +0530

    React Fragment in React JS use it instead of div in multiple elements

--- awesomeapp/src/index.js
+++ awesomeapp/src/index.js
@@ -6,12 +6,14 @@ import ReactDOM from 'react-dom';
 
 // ReactDOM.render('kya dikhana hai','kahan dikhana hai','callback func');
 ReactDOM.render(
-  //div or [ , ] is compulsary for multiple elements
-  [
-  <h1>Hello World!!</h1>,
-  <p>The world is here</p>
-  
-  ]
+  //div or [ , ] if version above 16 is compulsary for multiple elements
+
+  //We should use React fragment instead of div if version is before 16 as div will create extra div
+
+  <React.Fragment>
+    <h1>Hello World!!</h1>
+    <p>The world is here</p>
+  </React.Fragment>
 
   ,
 
