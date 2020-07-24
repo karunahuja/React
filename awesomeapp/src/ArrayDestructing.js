444ba83309bc7d1ca71384e19d5949b6f601bb56
karun <karunahuja1998@gmail.com>
2020-07-25 02:44:32 +0530

    Array Destructuring in ES6

---
+++ awesomeapp/src/ArrayDestructing.js
@@ -0,0 +1,20 @@
+const myproglang=['js','php','c','python'];
+
+var top1=myproglang[0]
+var top2=myproglang[1]
+var top3=myproglang[2]
+
+console.log("My fav is "+top1);
+
+
+
+let [top1,top2,top3,top4,top5]=myproglang;
+console.log("my fav is "+top1);
+
+
+console.log(myproglang.length);
+
+let [top1,,,,toplast]=myproglang;
+console.log("my least fav is "+toplast);
+
+
