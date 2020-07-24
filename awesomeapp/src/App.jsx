9732a490949039e7ad979a8e5ddb5f3d1de9551d
karun <karunahuja1998@gmail.com>
2020-07-25 03:59:13 +0530

    Create a Digital Clock using React JS

--- awesomeapp/src/App.jsx
+++ awesomeapp/src/App.jsx
@@ -8,6 +8,7 @@ const App=()=>{
     // const [vi,no,d]=name;
     
     // const [name1,name2,name3]=name;  
+ 
     
 const state=useState();//return current data and updated data
 const[count,setCount]=useState(date);//0 is initial value, count is current value(0) initially,set count is updated value and updated value becomes current value of count
@@ -19,11 +20,14 @@ const[count,setCount]=useState(date);//0 is initial value, count is current valu
         //  will not applicable in react       count++;
         
     }
+    setInterval(() => {
+        IncNum()
+    }, 1000);
     
     return(
         <>
         <h1>{count}</h1>
-        <button onClick={IncNum}>Click me</button>
+      
         </>
     )
 
