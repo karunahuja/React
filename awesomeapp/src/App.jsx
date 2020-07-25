58991ba3065adc6de78b232c9f294375b700fb09
karun <karunahuja1998@gmail.com>
2020-07-26 03:00:50 +0530

    Building a Todo List, it is complex

--- awesomeapp/src/App.jsx
+++ awesomeapp/src/App.jsx
@@ -1,100 +1,65 @@
 import React, { useState } from 'react';
-
+import ToDoList from "./TodoList"
 
 const App = () => {
 
-    const [fullname, setFullName] = useState({
 
-        fname: "",
-        lname: "",
-        email: "",
-        phone: "",
+    const[inputList,setInputList] =useState("");
+    const[Items,setItems]=useState([]);
 
+    const itemEvent=(event)=>{
+        setInputList(event.target.value)
+    }
 
-    });
-
-
-
-
-
-
-    const InputEvent = (event) => {
-        console.log(event.target.value);
-        const value = event.target.value;
-        const name = event.target.name;
-
-        setFullName((preValue) => {
-        return{
-                    ...preValue,
-                    [name]:value
-
-        }
-            // if (name === "fname") {
-            //     return {
-            //         fname: value,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-
-            //     }
-            // } else if (name === "lname") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: value,
-            //         email: preValue.email,
-            //         phone: preValue.phone
-            //     }
-            // }
-
-            // else if (name === "email") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: value,
-            //         phone: preValue.phone
-            //     }
-
-            // } else if (name === "phone") {
-            //     return {
-            //         fname: preValue.fname,
-            //         lname: preValue.lname,
-            //         email: preValue.email,
-            //         phone: value
-            //     }
-            // }
-
-
-
+    const listOfItems=()=>{
+        setItems((oldItems)=>{
+            return[...oldItems,inputList]
         })
 
+        setInputList("");
+    }
+    const deleteItems=(id)=>{
+
+        setItems((oldItems)=>{
+            return oldItems.filter((arrElem,index)=>{
+                return index!=id;
+            })
+        })
+        
+    
     }
 
+    return(
+    <>
+<div className="main_div">
+        <div className="center_div">
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+        
+            <h1>To Do list</h1>
+        
+        <input type="text" placeholder="Add a items" onChange={itemEvent} value={inputList}/>
 
-        alert("Form Submitted")
+        <button onClick={listOfItems}>+</button>
+        <ol>
+            {/* <li>{inputList}</li> */}
+          {  
+              Items.map((itemval,index)=>{
+             return( <ToDoList key={index}
+             id={index}
+                text={itemval}
+                onSelect={deleteItems}
+              ></ToDoList>)
 
-    }
+         })}
+           
+        </ol>
 
-    return (
-        <>
-            <div>
+    </div>
+    </div>
 
-                <form onSubmit={onSubmit}>
-                    <h1>Hello {fullname.fname}{fullname.lname}
-                        {fullname.email}{fullname.phone}
-                    </h1>
-                    <input type="text" name="fname" placeholder="Enter your name" value={fullname.fname} onChange={InputEvent} /*value="name" can't use value*/></input>
-                    <input type="text" name="lname" placeholder="Enter your last Name" value={fullname.lname} onChange={InputEvent} /*value="name" can't use value*/></input>
-                    <input type="email" name="email" placeholder="Enter your email" value={fullname.email} onChange={InputEvent} /*value="name" can't use value*/></input>
-                    <input type="text" name="phone" placeholder="Enter your phone" value={fullname.phone} onChange={InputEvent} /*value="name" can't use value*/></input>
-                    <button type="submit" >Click me</button>
-                </form>
-            </div>
-        </>
+
+    </>
     )
-
 }
 export default App;
 
