76a39173b6249a6513214a6fd0abd75a90183b38
karun <karunahuja1998@gmail.com>
2020-07-25 07:36:11 +0530

    Three dots ... in react

---
+++ awesomeapp/src/threedots.js
@@ -0,0 +1,11 @@
+const fullname=["karun", "Ahuja","good","boy"]
+
+const biodata=[1,25,...fullname,"male"];
+
+const [first,...remaining]=biodata
+
+console.log(fullname)
+console.log(biodata)
+console.log(first)
+console.log(remaining)
+
