bcd114fb49cba9977728e05dbe5e1c459f37f151
karun <karunahuja1998@gmail.com>
2020-07-28 07:50:56 +0530

    useContext Hook in React :- better approach to previous commit

--- awesomeapp/src/ComponentB.jsx
+++ awesomeapp/src/ComponentB.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{useContext} from 'react'
 
+import {FirstName,LastName} from "./App.jsx"
 import ComponentC from "./ComponentC";
 const ComponentB=()=>{
-
-    return <ComponentC></ComponentC>
+    const fname=useContext(FirstName);
+    const lname=useContext(LastName);
+    return (<h1>My name is {fname}{lname} </h1>)
 }
 
 export default ComponentB;
