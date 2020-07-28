723244683e852124278f0ee7a9b3e6a0e723947c
karun <karunahuja1998@gmail.com>
2020-07-28 13:10:20 +0530

    react api call to get pokemon json data using axios and useEffect

--- awesomeapp/src/App.jsx
+++ awesomeapp/src/App.jsx
@@ -1,38 +1,13 @@
 import React, { useState,useEffect} from 'react';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
+import ComA from "./ComA"
 const App = () => {
-   const [num,setNum]=useState(0);
-   const [nums,setNums]=useState(0);
-   
-   //use effect for num will be called everytime after render in index.js
-   useEffect(()=>{
-    alert("I am clicked");
-document.title=`you clickd me ${num} times`
-   })
-
+  
         return(
         <>
-        
-        <button onClick={()=>{setNum(num+1)
-
-
-        }
-        }>Click Me{num}</button>
-
-        <br/>
-
-        
-<button onClick={()=>{setNums(nums+1)
-
-
-}
-}>Click Me{nums}</button>
-
-
-       </> )
-
+        <ComA></ComA>
+       </>)
 
 }
 export default App;
