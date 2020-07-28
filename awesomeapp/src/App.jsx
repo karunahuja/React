93dc6a905f4018add371c9048e815cfa66c50d9c
karun <karunahuja1998@gmail.com>
2020-07-28 07:34:19 +0530

    Context API in React JS

--- awesomeapp/src/App.jsx
+++ awesomeapp/src/App.jsx
@@ -1,74 +1,28 @@
-import React, { useState } from 'react';
-import ToDoList from "./TodoList"
+import React, { createContext } from 'react';
+import ComponentA from './ComponentA'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const FirstName=createContext();
+const LastName=createContext();
+
 const App = () => {
+   
+        return(
+
+            <FirstName.Provider value={"Karun"}>
+            <LastName.Provider value={"Ahuja"}>
+            <ComponentA></ComponentA>
+            </LastName.Provider>
+ 
 
 
-    const[inputList,setInputList] =useState("");
-    const[Items,setItems]=useState([]);
+            </FirstName.Provider>
+            
+        )
 
-    const itemEvent=(event)=>{
-        setInputList(event.target.value)
-    }
 
-    const listOfItems=()=>{
-        setItems((oldItems)=>{
-            return[...oldItems,inputList]
-        })
-
-        setInputList("");
-    }
-    const deleteItems=(id)=>{
-
-        setItems((oldItems)=>{
-            return oldItems.filter((arrElem,index)=>{
-                return index!=id;
-            })
-        })
-        
-    
-    }
-
-    return(
-    <>
-<h1>Welcome to my channel</h1>
-<div className="container">
-  <div className="row">
-    <div className="col-sm">
-    <div class="card" >
-  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-    <div className="col-sm">
-    <div class="card" >
-  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-    <div className="col-sm">
-    <div class="card" >
-  <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px"/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-  </div>
-</div>
-    </>
-    )
 }
 export default App;
+export {FirstName,LastName};
 
 
---
+++ awesomeapp/src/ComponentA.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ComponentB from "./ComponentB";
+const ComponentA=()=>{
+
+    return <ComponentB></ComponentB>
+}
+
+export default ComponentA;
