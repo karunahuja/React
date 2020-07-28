711ba65251a0cd2b19778a6ec274ad01efd237d6
karun <karunahuja1998@gmail.com>
2020-07-28 21:12:24 +0530

    React navabar /Menu using router

--- awesomeapp/src/App.jsx
+++ awesomeapp/src/App.jsx
@@ -2,11 +2,13 @@ import React, { useState,useEffect} from 'react';
 import {Route,Switch} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import About from './About'
+import Menu from "./Menu"
 import Contact from './Contact'
 const App = () => {
   
         return(
         <>
+        <Menu></Menu>
         <Switch>
             <Route exact path="/" component={About} ></Route>
             <Route exact path="/contact" component={Contact} ></Route>
---
+++ awesomeapp/src/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+const Menu=()=>{
+
+return(
+<>
+<NavLink exact activeClassName="active_class" to="/">About Us</NavLink>
+<NavLink exact activeClassName="active_class" to="/contact">Contact Us</NavLink>
+<br/>
+<a href="/">About us</a>
+<a href="/contact">Contact</a>
+</>
+)
+
+}
+export default Menu;
