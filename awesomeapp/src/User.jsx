8601f0a84a067541676b6152a35809b14a53ca1d
karun <karunahuja1998@gmail.com>
2020-07-29 01:23:22 +0530

    useHistory Hook in React to go back for previous page

--- awesomeapp/src/User.jsx
+++ awesomeapp/src/User.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import{useParams,useLocation} from "react-router-dom"
+import{useParams,useLocation,useHistory} from "react-router-dom"
 
 const User=()=>{
     const {fname}=useParams();
     const location=useLocation();
+    const history=useHistory();
     return(
     <>
     <h1>user{fname}page</h1>
     <p>My current location is {location.pathname} </p>
         {location.pathname===`/contact/user/karunahuja`?(
-            <button>ClickMe</button>
+            <button onClick={()=>history.goBack()}>ClickMe</button>
         ):null}
 </>
     )
